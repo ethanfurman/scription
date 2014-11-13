9b06c228fe09c78f4fea5887f036c380451d5270
Ethan Furman <ethan@stoneleaf.us>
2014-11-13 15:11:30 -0800

    version 0.70.82

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Scription -- simple script parameter parser
 """
 
 setup( name='scription',
-       version= '0.70.82.alpha',
+       version= '0.70.82',
        license='BSD License',
        description='simple script parameter parser',
        long_description=long_desc,
