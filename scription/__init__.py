4dc7c9405fd051bf46e588f3420eb54414904cc3
Ethan Furman <ethan@stoneleaf.us>
2015-03-04 12:07:45 -0800

    version 0.74.04

--- scription/__init__.py
+++ scription/__init__.py
@@ -70,7 +70,7 @@ from sys import stdout, stderr
   - remove determines if this argument is removed from sys.argv
 """
 
-version = 0, 74, 3
+version = 0, 74, 4
 
 # data
 __all__ = (
--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ features
 '''
 
 setup( name='scription',
-       version= '0.74.03',
+       version= '0.74.04',
        license='BSD License',
        description='simple script parameter parser',
        long_description=description,
