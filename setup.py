e98b9066d41381ef95773a68967128dac3162604
Ethan Furman <ethan@stoneleaf.us>
2019-05-30 11:16:58 -0700

    version bump

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ make = []
 
 data = dict(
         name='scription',
-        version='0.82.0',
+        version='0.83.0',
         license='BSD License',
         description='simple script parameter parser',
         long_description=description,
