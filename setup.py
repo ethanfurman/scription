1551538ca907525c40d3c8d99a568db02a72481c
Ethan Furman <ethan@stoneleaf.us>
2023-03-29 13:01:41 -0700

    version 0.86.19

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ make = []
 
 data = dict(
         name='scription',
-        version='0.86.19a1',
+        version='0.86.19',
         license='BSD License',
         description='simple script parameter parser',
         long_description=description,
